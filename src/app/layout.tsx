2b5b536160844b19e86a46a35d8dfd245e08e6d7
David Silva <contatodavidmaia@gmail.com>
2023-11-15 19:29:35 -0300

    feat: add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,20 @@ const roboto = Roboto({ subsets: ['latin'], weight: ['500', '700', '900'] })
 export const metadata: Metadata = {
   title: 'Polaroider',
   description: 'A simple app to create Polaroid photos.',
+  openGraph: {
+    siteName: 'Polaroider',
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://polaroider.vercel.app/og-image.png',
+    images: [
+      {
+        url: '',
+        width: 1200,
+        height: 630,
+        alt: 'Polaroider',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
